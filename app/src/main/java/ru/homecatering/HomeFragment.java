8b5e58ed17c7c1a3c5b4bd209ee8dd9c1eb4701e
AndreevIlya <katauri@yandex.ru>
2019-05-27 15:41:43 +0300

    Temperature and humidity sensors are done. They produce strange results.

--- app/src/main/java/ru/homecatering/HomeFragment.java
+++ app/src/main/java/ru/homecatering/HomeFragment.java
@@ -1,17 +1,114 @@
 package ru.homecatering;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import static android.content.Context.SENSOR_SERVICE;
 
 public class HomeFragment extends Fragment {
+
+    private TextView textConsole;
+    private TextView textTemperature;
+    private TextView textHumidity;
+    private SensorManager sensorManager;
+    private List<Sensor> sensors;
+    private Sensor sensorTemperature;
+    private Sensor sensorHumidity;
+
+    SensorEventListener listenerTemperature = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            showTemperatureSensors(event);
+        }
+    };
+
+    SensorEventListener listenerHumidity = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            showHumiditySensors(event);
+        }
+    };
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        textConsole = view.findViewById(R.id.textConsole);
+        textTemperature = view.findViewById(R.id.textTemperature);
+        textHumidity = view.findViewById(R.id.textHumidity);
+        sensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
+        sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        sensorTemperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        sensorManager.registerListener(listenerTemperature, sensorTemperature,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorHumidity = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        sensorManager.registerListener(listenerHumidity, sensorHumidity,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        showSensors();
+        return view;
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listenerTemperature, sensorTemperature);
+        sensorManager.unregisterListener(listenerHumidity, sensorHumidity);
+    }
+
+
+    private void showSensors() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Sensor sensor : sensors) {
+            stringBuilder.append("name = ").append(sensor.getName())
+                    .append(", type = ").append(sensor.getType())
+                    .append("\n")
+                    .append("vendor = ").append(sensor.getVendor())
+                    .append(" ,version = ").append(sensor.getVersion())
+                    .append("\n")
+                    .append("max = ").append(sensor.getMaximumRange())
+                    .append(", resolution = ").append(sensor.getResolution())
+                    .append("\n").append("--------------------------------------").append("\n");
+        }
+        textConsole.setText(stringBuilder);
+    }
+
+    private void showTemperatureSensors(SensorEvent event) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(getActivity().getResources().getString(R.string.temperature_note)).append(event.values[0])
+                .append("\n").append("=======================================").append("\n");
+        Log.i("Info", stringBuilder.toString());
+        textTemperature.setText(stringBuilder);
+    }
+
+    private void showHumiditySensors(SensorEvent event) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(getActivity().getResources().getString(R.string.humidity_note)).append(event.values[0])
+                .append("\n").append("=======================================").append("\n");
+        Log.i("Info", stringBuilder.toString());
+        textHumidity.setText(stringBuilder);
+    }
+
+
 }
